82de7c1a6ebdfbcc81ed387835b13eed19b103cf
DxB <dxb@boardmansgame.com>
2026-02-03 07:30:54 +0000

    Add script/ for management

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['.boardmansgame.com'],
+    allowedHosts: ['.boardmansgame.com','.midnighttree.com'],
     port: 3000,
     open: true
   }
